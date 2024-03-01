6b23fb1454451dd8cb41d2261012855a77454b92
Chiranjeev Thapliyal <6258860@gmail.com>
2024-03-01 15:08:34 +0530

    Initial Commit

---
+++ Hike/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Chiranjeev Thapliyal on 01/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Chiranjeev Thapliyal on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
